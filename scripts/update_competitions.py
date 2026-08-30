26511f88b66ee1942cad2cdbf4e1e365f6bc1fff
jokerui666 <joker@stu.edu.tw>
2026-08-30 14:05:59 +0800

    Update update_competitions.py

--- scripts/update_competitions.py
+++ scripts/update_competitions.py
@@ -48,17 +48,29 @@ session.headers.update(HEADERS)
 
 MONTHS = {
     "january": 1,
+    "jan": 1,
     "february": 2,
+    "feb": 2,
     "march": 3,
+    "mar": 3,
     "april": 4,
+    "apr": 4,
     "may": 5,
     "june": 6,
+    "jun": 6,
     "july": 7,
+    "jul": 7,
     "august": 8,
+    "aug": 8,
     "september": 9,
+    "sep": 9,
+    "sept": 9,
     "october": 10,
+    "oct": 10,
     "november": 11,
+    "nov": 11,
     "december": 12,
+    "dec": 12,
 }
 
 
@@ -524,8 +536,9 @@ def parse_date(text):
 
     pattern_1 = re.compile(
         r"\b"
-        r"(January|February|March|April|May|June|July|August|"
-        r"September|October|November|December)"
+        r"(January|Jan|February|Feb|March|Mar|April|Apr|May|June|Jun|"
+        r"July|Jul|August|Aug|September|Sep|Sept|October|Oct|"
+        r"November|Nov|December|Dec)"
         r"\s+"
         r"(\d{1,2})"
         r"(?:st|nd|rd|th)?"
@@ -570,8 +583,9 @@ def parse_date(text):
         r"(\d{1,2})"
         r"(?:st|nd|rd|th)?"
         r"\s+"
-        r"(January|February|March|April|May|June|July|August|"
-        r"September|October|November|December)"
+        r"(January|Jan|February|Feb|March|Mar|April|Apr|May|June|Jun|"
+        r"July|Jul|August|Aug|September|Sep|Sept|October|Oct|"
+        r"November|Nov|December|Dec)"
         r"(?:,\s*|\s+)"
         r"(2026|2027)"
         r"\b",
